f1fd1d184be3218e1628de39b34a0c5bea88cc24
Chris Santella <santella.chris@gmail.com>
2024-11-13 23:38:58 -0500

    Add helper shell scripts, update .gitignore

---
+++ start-pgadmin4-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$0" != "./start-pgadmin4-docker.sh" ]]; then
+    echo "This script must be run from within the project folder"
+    echo "Dolla 0: $0"
+    exit 1
+fi
+
+PGADMIN_DEFAULT_PASSWORD="$(cat .env-secret)"
+
+docker pull dpage/pgadmin4:latest
+
+docker run -p 5050:80 \
+	-e "PGADMIN_DEFAULT_EMAIL=santella.chris@gmail.com" \
+	-e "PGADMIN_DEFAULT_PASSWORD=$PGADMIN_DEFAULT_PASSWORD" \
+	--name local-pgadmin \
+	-d dpage/pgadmin4
